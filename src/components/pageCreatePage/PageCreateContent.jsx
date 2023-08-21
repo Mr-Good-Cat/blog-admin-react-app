7f210591d50969f788fcd329691d6c3ea7dad4b0
ivan.sankov <ivan.sankov@skillguilds.com>
2023-08-21 15:51:13 +0700

    move code of form to separate file

---
+++ src/components/pageCreatePage/PageCreateContent.jsx
@@ -0,0 +1,13 @@
+import PageCreateForm from "./PageCreateForm";
+
+function PageCreateContent() {
+  return (
+    <div className="bg-white p-8">
+      <h2 className="text-xl font-bold mb-2">Create page</h2>
+
+      <PageCreateForm />
+    </div>
+  );
+}
+
+export default PageCreateContent;
--- src/components/pages/PageCreatePage.jsx
+++ src/components/pages/PageCreatePage.jsx
@@ -1,48 +1,7 @@
-import Input from "../commons/Input";
-import Dropdown from "../commons/Dropdown";
-import Textarea from "../commons/Textarea";
+import PageCreateContent from "../pageCreatePage/PageCreateContent";
 
 function PageCreatePage() {
-  return (
-    <div className="bg-white p-8">
-      <form action="">
-        <div className="lg:flex lg:justify-between">
-          <div className="lg:w-2/5 mb-2">
-            <Input label="Title" />
-          </div>
-          <div className="lg:flex lg:justify-between lg:w-2/5">
-            <div className="lg:w-1/3 mb-2">
-              <Input label="Order" />
-            </div>
-
-            <div className="lg:w-1/3 mb-2">
-              <Dropdown
-                label="Type"
-                options={[{ id: 1, value: "qq", title: "qwe" }]}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="lg:flex lg:justify-between">
-          <div className="lg:w-2/5 mb-2">
-            <Input label="Slug" />
-          </div>
-
-          <div className="lg:w-2/5">
-            <div className="mb-2">
-              <Input label="Seo Title" />
-            </div>
-            <div className="mb-2">
-              <Input label="Seo Description" />
-            </div>
-          </div>
-        </div>
-
-        <Textarea label="Description" />
-      </form>
-    </div>
-  );
+  return <PageCreateContent />;
 }
 
 export default PageCreatePage;
